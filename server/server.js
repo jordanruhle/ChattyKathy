461638de0614873a2a95b6d7a661d9afe7b0606b
Jordan Ruhle <jordanruhle@gmail.com>
2023-04-21 16:18:17 -0700

    added to server.js

--- server/server.js
+++ server/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const app = express();
+const cors = require('cors')
 const dotenv = require('dotenv');
 dotenv.config({ path: '../.env' });
+
+// middleware
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
+app.use(express.json(), express.urlencoded({extended:true}));
+app.use(express.static(process.env.STATIC_DIR));
+
+app.listen(port, () => console.log(`Listening on port: ${port}`) );
