aeaaedc59946ef5363c80b55e04df6afbd787186
Jordan Ruhle <jordanruhle@gmail.com>
2023-04-22 08:42:41 -0700

    set up models

--- server/config/sequelize.config.js
+++ server/config/sequelize.config.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 
 // Create an instance of sequelize
 const sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
-  host: 'db',
+  host: process.env.DB_HOST,
   dialect: 'postgres',
   logging: false,
 });
